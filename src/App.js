619d6ef37167bb26474480f73e4ff053b68e4118
Panthutk <panthat.k@gmail.com>
2022-11-10 01:03:17 +0700

    login log

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 import './App.css';
+import { useState,useEffect } from "react";
 
 import {Sidebar} from './components/Sidebar'
 import { Login } from './components/Login'
 function App() {
   axios.post('http://165.22.3.172:9999/users', {
-    firstName: 'Fred',
-    lastName: 'Flintstone'
   })
   .then(function (response) {
     console.log(response);
@@ -14,6 +13,8 @@ function App() {
   .catch(function (error) {
     console.log(error);
   });
+  
+  
   axios.get("http://165.22.3.172:9999/users").then((res)=>console.log(res.data)) 
   return (
     <div className="App">
